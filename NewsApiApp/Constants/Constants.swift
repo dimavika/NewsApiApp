10ac146a4578240326494118e99788c0155621a6
dimavi_ka <artes2222@yandex.ru>
2021-04-02 12:00:47 +0300

    Podfile

--- NewsApiApp/Constants/Constants.swift
+++ NewsApiApp/Constants/Constants.swift
@@ -15,7 +15,7 @@ struct Constants {
     static let tintColor = UIColor(red: 237.0/255.0, green: 101.0/255.0, blue: 106.0/255.0, alpha: 1.0)
     
     //MARK: -URL
-    static let url = "https://newsapi.org/v2/everything?q=apple&sortBy=popularity&apiKey=6ca6b4975a384c0495d5ec6863d59c12"
+    static let url = "https://newsapi.org/v2/everything?q=apple&language=en&sortBy=popularity&apiKey=6ca6b4975a384c0495d5ec6863d59c12"
     
     //MARK: -Error messages
     static let updateNewsErrorTitle = "Failed to update news"
